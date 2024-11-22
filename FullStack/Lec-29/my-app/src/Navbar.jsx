a7ec84ebd9fa22252fde6d5fe3d9db06e6382988
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2024-11-22 13:50:16 +0530

    created and added Navbar in react

---
+++ FullStack/Lec-29/my-app/src/Navbar.jsx
@@ -0,0 +1,21 @@
+import './css/Navbar.css';
+
+function Navbar(){
+    return (
+        <>
+        <div class="nav-bar">
+    <h1>Ankur Sinsinwar</h1>
+    <div class="link">
+
+        <a href="/">Our Story</a>
+        <a href="/">Membership</a>
+        <a href="/">Write</a>
+        <a href="/">Sign in</a>
+        <button id="btn">Get Started</button>
+    </div>
+</div>
+</>
+    );
+}
+
+export default Navbar;
---
+++ FullStack/Lec-29/my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Navbar from './Navbar';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Navbar />,
+  <h1>Hello</h1>
+);
+
+
