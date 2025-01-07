85fa8d4f81cc5ef0d2690a542982026f660fa545
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2025-01-07 12:48:49 +0530

    express lec2 as Lec-31

--- FullStack/Lec-31/FirstExpressApp/index.js
+++ FullStack/Lec-31/FirstExpressApp/index.js
@@ -4,16 +4,29 @@ const app = express();
 
 app.get('/',(req,res)=>{
     res.send('<h1>OK!</h1>')
-})
+});
 
 
 app.get('/about',(req,res)=>{
     res.send('This is about page!')
-})
+});
 
 app.get('/projects',(req,res)=>{
     res.send('This is project page!')
-})
+});
 
 
-app.listen(4000)
+app.get('/login',(req,res)=>{
+    res.send('Login')
+});
+
+app.get('/signup/:username/:password',(req,res)=>{
+    console.log(req.params)
+    res.send('This is signup page!')
+});
+
+
+
+app.get('/*',(req,res)=>{
+    res.send('<h1>404 page not found</h1>')
+}); app.listen(4000);
