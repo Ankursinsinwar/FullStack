ea710d44a105023203e9af6ae8556b920c704bde
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2025-01-16 15:21:56 +0530

    Lec - 34 - ejs-edit-

--- FullStack/Lec-34/restfull/data/user.js
+++ FullStack/Lec-34/restfull/data/user.js
@@ -1,29 +1,34 @@
 let Users = [
     {
+        id:1,
         name: 'John Doe',
         email: 'johndoe@example.com',
         password: '123',
         city: 'New York'
     },
     {
+        id:2,
         name: 'Jane Smith',
         email: 'janesmith@example.com',
         password: 'abc123',
         city: 'Los Angeles'
     },
     {
+        id:3,
         name: 'Robert Brown',
         email: 'robertbrown@example.com',
         password: 'pass456',
         city: 'Chicago'
     },
     {
+        id:4,
         name: 'Emily Davis',
         email: 'emilydavis@example.com',
         password: 'emily789',
         city: 'Houston'
     },
     {
+        id:5,
         name: 'Michael Johnson',
         email: 'michaeljohnson@example.com',
         password: 'mike123',
--- FullStack/Lec-34/restfull/index.js
+++ FullStack/Lec-34/restfull/index.js
@@ -1,37 +1,40 @@
 const express = require('express');
 const app = express();
 
-const Users = require('./data/user');
-const e = require('express');
+const Users = require('./data/user')
 
 app.set('view engine','ejs');
-
-app.use(express.urlencoded({ extended: true }));
-
-
+app.use(express.urlencoded({extended:true}))
+ 
 app.get('/',(req,res)=>{
-    res.send('home page')
-});
-
+    res.send('HOME PAGE')
+})
 
 app.get('/users',(req,res)=>{
     res.render('user',{Users})
-});
-
+})
 
 app.get('/user/new',(req,res)=>{
-    res.render('new',{Users})
-});
+    res.render('new');
+})
 
 app.post('/users',(req,res)=>{
-    const {name,password,email,city} = req.body;
-    let user = {name,password,email,city};
+    const {username,email,city,password} = req.body;
+
+    let id = Users[Users.length-1].id +1;
+
+    const user = {id,username,password,city,email};
     Users.push(user);
-    res.redirect('/users')
-});
+    res.redirect('/users');
+})
 
+app.get('/users/:id',(req,res)=>{
+    const {id} = req.params;
 
-const port = 4000;
-app.listen(port,()=>{
-    console.log('Server is running on port', port);
+    let user = Users.find((user)=>user.id==id);
+    res.render('show',{user});
+})
+
+app.listen(4000,()=>{
+    console.log('server run at port 4000');
 })
