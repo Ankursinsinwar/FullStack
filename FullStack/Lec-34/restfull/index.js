ed165b205a8467a1e787fb4ac57842357265b3d9
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2025-01-16 14:47:10 +0530

    Lec - 34 - ejs-post-

--- FullStack/Lec-34/restfull/index.js
+++ FullStack/Lec-34/restfull/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 
 const Users = require('./data/user');
+const e = require('express');
 
 app.set('view engine','ejs');
+
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/',(req,res)=>{
@@ -11,7 +14,7 @@ app.get('/',(req,res)=>{
 });
 
 
-app.get('/user',(req,res)=>{
+app.get('/users',(req,res)=>{
     res.render('user',{Users})
 });
 
@@ -20,8 +23,15 @@ app.get('/user/new',(req,res)=>{
     res.render('new',{Users})
 });
 
+app.post('/users',(req,res)=>{
+    const {name,password,email,city} = req.body;
+    let user = {name,password,email,city};
+    Users.push(user);
+    res.redirect('/users')
+});
+
 
 const port = 4000;
 app.listen(port,()=>{
-    console.log('Server is running on port', {port});
+    console.log('Server is running on port', port);
 })
